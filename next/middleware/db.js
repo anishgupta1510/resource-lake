35cdc2b009890f0125c017d474120c34192e637e
anish <anishgupta700@gmail.com>
2023-06-06 13:51:00 +0530

    commit

--- next/middleware/db.js
+++ next/middleware/db.js
@@ -4,7 +4,7 @@ const connectDB = handler => async(req,res) => {
     if(mongoose.connections[0].readyState){
         return handler(req,res);
     }
-    await mongoose.connect(process.env.MONGO_URI,{
+    await mongoose.connect("mongodb+srv://anish:anish@mern.jdkgghn.mongodb.net/food?retryWrites=true&w=majority",{
         useUnifiedTopology:true,
         useNewUrlParser:true
     });
